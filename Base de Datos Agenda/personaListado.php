d5be25ce6aaf18ba17182e4120d3d77c97e50127
Juanma Ramiro <22/08/1992>
2020-11-12 13:11:28 +0100

    Eliminación enlaces estrella
    Eliminados los <a> de la fila estrella

--- Base de Datos Agenda/personaListado.php
+++ Base de Datos Agenda/personaListado.php
@@ -47,12 +47,10 @@ $rs = $select->fetchAll();
             <?php
             if ($fila["pEstrella"]) {
                 $urlImagen = "img/estrellaRellena.png";
-                $parametroEstrella = "estrella";
             } else {
                 $urlImagen = "img/estrellaVacia.png";
-                $parametroEstrella = "";
             }
-            echo " <td style='text-align: center'><a href='personaEstablecerEstadoEstrella.php?$parametroEstrella'><img src='$urlImagen' width='16' height='16'></a></td>";
+            echo " <td style='text-align: center'><img src='$urlImagen' width='16' height='16'></td>";
             ?>
             <td style='text-align: center'><a href='personaEliminar.php?id=<?=$fila["pId"]?>'> (X) </a></td>
         </tr>
