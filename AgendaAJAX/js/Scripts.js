1c8848ac16da50494eddc158a293cca89e057db9
Juanma Ramiro <22/08/1992>
2021-02-04 13:49:34 +0100

    Agenda AJAX

---
+++ AgendaAJAX/js/Scripts.js
@@ -0,0 +1,18 @@
+window.onload = inicializaciones;
+
+function inicializaciones() {
+
+}
+
+function cargarTodasLasCategorias() {
+    // TODO v0.9 Obtener el JSON con UNA categoría.
+    // TODO v1.0 Obtener el JSON con un ARRAY de categorías.
+
+    // TODO Adaptar/traducir esto a Javascript/DOM/etc.
+    // <?php foreach ($categorias as $categoria) { ?>
+    //     <tr>
+    // <td><a href='CategoriaFicha.php?id=<?=$categoria->getId()?>'>    <?=$categoria->getNombre()?> </a></td>
+    // <td><a href='CategoriaEliminar.php?id=<?=$categoria->getId()?>'> (X)                            </a></td>
+    // </tr>
+    // <?php } ?>
+}
